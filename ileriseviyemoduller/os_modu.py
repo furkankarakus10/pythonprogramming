ffe5a0c37860291a9a176869b48156ab0c6c2385
furkankarakus10 <furkan.karakus@icloud.com>
2023-12-22 10:41:20 +0300

    22-12-2023 gün ortası resart

--- ileriseviyemoduller/os_modu.py
+++
@@ -1,5 +0,0 @@
-import os
-
-##print(dir(os))
-
-print(os.getcwd())
---
+++ ileriseviyemoduller/os_modulu/os_modu.py
@@ -0,0 +1,55 @@
+import os
+
+##print(dir(os))
+
+"""
+print("\n")
+os.chdir("D:")
+print(os.getcwd())
+print("\n")
+print(os.listdir())
+for i in os.listdir():
+    print(i)
+
+print("\n")
+print(os.getcwd())
+##os.mkdir("Deneme1")
+os.makedirs("Deneme2/Deneme3")
+
+print("\n")
+print(os.getcwd())
+##os.rmdir("Deneme2/Deneme3")
+##os.mkdir("Deneme2/Deneme3")
+##os.removedirs("Deneme2/Deneme3")
+"""
+print("\n")
+print(os.getcwd())
+#f = open("text.txt","x")
+#f = open("text.txt","w")
+#f.write("bu bir deneme dosyasıdır.")
+#os.rename("text.txt","text2.txt")
+print(os.stat("text2.txt"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
